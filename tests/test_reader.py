4caff0a87648401da5b67d3bc42516108683ac41
Arturo Martínez <arturo.mtz8@gmail.com>
2023-04-30 21:08:35 +0000

    scrape daily posts from subreddits

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -4,10 +4,8 @@ from pathlib import Path
 
 sys.path.insert(0, (os.path.join(os.path.dirname(__file__), "..")))
 
-from src.flows.gc_funcs.reader_writer import (  # noqa: E402
-    get_posts_from_gcs,
-    write_to_gcs,
-)
+from src.flows.gc_funcs.reader_writer import (get_posts_from_gcs,  # noqa: E402
+                                              write_to_gcs)
 
 df_test_posts = get_posts_from_gcs()
 # df_test_comments = get_comments_from_gcs()
